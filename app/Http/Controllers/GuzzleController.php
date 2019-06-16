bcb4c798d515739be3190072f6f6cdc99b57e4aa
Dejan Jevtić <dejan_d_jevtic@yahoo.com>
2019-06-16 15:15:29 +0200

    Add API and get Data

--- app/Http/Controllers/GuzzleController.php
+++ app/Http/Controllers/GuzzleController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class GuzzleController extends Controller
 {
     public function getRemoteData() {
-    	return 'something';
+    	$client = new Client([
+    		'headers' =>['content-type' =>'application/json','Accept'=>'application/json'],
+    		]);
+
+    		$response = $client->request('GET','https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo',[
+
+    			// 'json' => [
+    			// 	'code'=>
+    			// ]
+
+    		]);
+    		$data = $response->getBody();
+    		$data = json_decode($data);
+    		dd($data);
     }
 }
