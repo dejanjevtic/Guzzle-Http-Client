bad1afbeaaea0ce0cc21c2a1b455f3efde38d6bb
Dejan Jevtić <dejan_d_jevtic@yahoo.com>
2019-06-16 14:20:40 +0200

    Guzzle Controller

---
+++ app/Http/Controllers/GuzzleController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GuzzleController extends Controller
+{
+    public function getRemoteData() {
+    	return 'something';
+    }
+}
